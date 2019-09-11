e02bb5c9fde32ddce38eec2da3d683f37e9b2dd5
mshaileshr@gmail.com <Yes@383840>
2019-09-11 17:35:26 +0530

    initial commit

--- app/src/main/java/com/allandroidprojects/ecomsample/product/ItemDetailsActivity.java
+++ app/src/main/java/com/allandroidprojects/ecomsample/product/ItemDetailsActivity.java
@@ -17,16 +17,19 @@ import com.allandroidprojects.ecomsample.utility.ImageUrlUtils;
 import com.facebook.drawee.view.SimpleDraweeView;
 
 public class ItemDetailsActivity extends AppCompatActivity {
+
     int imagePosition;
     String stringImageUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_details);
-        SimpleDraweeView mImageView = (SimpleDraweeView)findViewById(R.id.image1);
-        TextView textViewAddToCart = (TextView)findViewById(R.id.text_action_bottom1);
-        TextView textViewBuyNow = (TextView)findViewById(R.id.text_action_bottom2);
+        SimpleDraweeView mImageView = findViewById(R.id.image1);
+        TextView textViewAddToCart = findViewById(R.id.text_action_bottom1);
+        TextView textViewBuyNow = findViewById(R.id.text_action_bottom2);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         //Getting image uri from previous screen
         if (getIntent() != null) {
             stringImageUri = getIntent().getStringExtra(ImageListFragment.STRING_IMAGE_URI);
